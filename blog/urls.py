88f5d7195a79db0a158df2e05788861126abc02b
anamaria <gomesabam@gmail.com>
2022-02-17 12:20:19 -0300

    Added template fot detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path #importando função url
-from . import views #importando todas as view do app blog, ainda ser criadas
+from . import views #importando todas as views do app blog, ainda ser criadas
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
